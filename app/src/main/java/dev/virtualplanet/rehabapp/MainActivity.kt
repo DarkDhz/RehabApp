7319ababdd6fd3465faa1efb9ced46446060fadf
VirtualPlanetNT <wDN%JD!:$$*+A6eZ4E}u-Sr>
2020-03-10 09:31:31 +0100

    create project

--- app/src/main/java/dev/virtualplanet/rehabapp/MainActivity.kt
+++ app/src/main/java/dev/virtualplanet/rehabapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package dev.virtualplanet.rehabapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToProfileActivity(view: View) {
+        val intent = Intent(this, ProfileActivity::class.java)
+        startActivity(intent)
+
     }
 }
--- app/src/main/java/dev/virtualplanet/rehabapp/ProfileActivity.kt
+++ app/src/main/java/dev/virtualplanet/rehabapp/ProfileActivity.kt
@@ -1,4 +1,13 @@
 package dev.virtualplanet.rehabapp
 
-class ProfileActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+
 }
