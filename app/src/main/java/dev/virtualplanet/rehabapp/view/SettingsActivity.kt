ff2c424e1b81b957f3400757d90ccad87d41deca
VirtualPlanetNT <wDN%JD!:$$*+A6eZ4E}u-Sr>
2020-03-14 20:49:32 +0100

    A-07-Disabled Shared Preferences, Added return button in ProfileActivity, configured EditProfileActivity

---
+++ app/src/main/java/dev/virtualplanet/rehabapp/view/SettingsActivity.kt
@@ -0,0 +1,16 @@
+package dev.virtualplanet.rehabapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.virtualplanet.rehabapp.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+
+    }
+
+}
