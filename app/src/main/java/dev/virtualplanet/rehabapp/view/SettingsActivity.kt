8d17aa3978538463656f0b0f3e9934e96222b42b
DarkDhz <blazegammer24@gmail.com>
2020-03-17 18:54:07 +0100

    A-13-Making view more Beautiful

--- app/src/main/java/dev/virtualplanet/rehabapp/view/SettingsActivity.kt
+++ app/src/main/java/dev/virtualplanet/rehabapp/view/SettingsActivity.kt
@@ -22,18 +22,18 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     fun modifyNotifications(view: View) {
-        if (view.stt_notify_bt.text.equals("ON")) {
-            view.stt_notify_bt.text = "OFF"
+        if (view.stt_notify_bt.text.equals("ON ")) {
+            view.stt_notify_bt.text = "OFF "
         } else {
-            view.stt_notify_bt.text = "ON"
+            view.stt_notify_bt.text = "ON "
         }
     }
 
     fun modifySound(view: View) {
-        if (view.stt_sound_bt.text.equals("ON")) {
-            view.stt_sound_bt.text = "OFF"
+        if (view.stt_sound_bt.text.equals("ON ")) {
+            view.stt_sound_bt.text = "OFF "
         } else {
-            view.stt_sound_bt.text = "ON"
+            view.stt_sound_bt.text = "ON "
         }
     }
 
