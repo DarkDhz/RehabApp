372989bcced39911cbc4ce53fc849495d8e8e0bd
EclaireC <joan.badia.andreu@gmail.com>
2020-03-22 00:55:20 +0100

    01-Joan:Intent getExtra

--- app/src/main/java/dev/virtualplanet/rehabapp/view/PlayExerciceActivity.kt
+++ app/src/main/java/dev/virtualplanet/rehabapp/view/PlayExerciceActivity.kt
@@ -3,6 +3,7 @@ package dev.virtualplanet.rehabapp.view
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import dev.virtualplanet.rehabapp.R
 
@@ -12,6 +13,17 @@ class PlayExerciceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_exercice)
     }
+    var hombro= intent.getStringExtra("hombro_derecho")
+    var hombroi=intent.getStringExtra("hombro_izquiero")
+    var codo= intent.getStringExtra("hombro_derecho")
+    var codo1=intent.getStringExtra("hombro_izquiero")
+    var muneca= intent.getStringExtra("hombro_derecho")
+    var munecai=intent.getStringExtra("hombro_izquiero")
+    var rodilla= intent.getStringExtra("hombro_derecho")
+    var rodillai=intent.getStringExtra("hombro_izquiero")
+    var tobillo= intent.getStringExtra("hombro_derecho")
+    var tobilloi=intent.getStringExtra("hombro_izquiero")
+    val resultTV=findViewById<TextView>(R.id.ce_title_txt)
 
     fun returnToMain(view: View) {
         val intent = Intent(this, MainActivity::class.java)
