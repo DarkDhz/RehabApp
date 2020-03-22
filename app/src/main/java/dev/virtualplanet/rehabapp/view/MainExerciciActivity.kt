639e403687c828b34c6b2e0138a7e25f002c7e13
VirtualPlanetNT <wDN%JD!:$$*+A6eZ4E}u-Sr>
2020-03-22 22:51:11 +0100

    Minnor Fixes

--- app/src/main/java/dev/virtualplanet/rehabapp/view/MainExerciciActivity.kt
+++ app/src/main/java/dev/virtualplanet/rehabapp/view/MainExerciciActivity.kt
@@ -1,20 +1,13 @@
 package dev.virtualplanet.rehabapp.view
 import android.app.ListActivity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import dev.virtualplanet.rehabapp.R
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.Spinner
 
 import dev.virtualplanet.rehabapp.controller.Controller
-import java.lang.AssertionError
-import java.lang.reflect.Array
 
 
 class MainExerciciActivity : ListActivity() {
@@ -30,21 +23,6 @@ class MainExerciciActivity : ListActivity() {
 
     }
 
-    fun startExercices(view: View) {
-        val intent = Intent(this, ViewExercicesActivity::class.java)
-        /*val option = findViewById<Spinner>(R.id.spinner)
-        if (option.selectedItemPosition > 1) {
-            intent.putExtra("Full-Body", "fullbody")
-
-        } else {
-            intent.putExtra("Rodilla", "rodilla")
-
-        }
-        startActivity(intent)
-        */
-
-    }
-
     fun goToMuscle(view: View) {
         val intent = Intent(this, selMus2Activity::class.java)
         startActivityForResult(intent,1234)
@@ -52,8 +30,8 @@ class MainExerciciActivity : ListActivity() {
 
 
     fun ListarMusclus(){
-        var adapt : Adaptador = Adaptador(this, musculos)
-        listAdapter=adapt
+        var adapt = Adaptador(this, musculos)
+        listAdapter = adapt
         l.setOnItemClickListener(AdapterView.OnItemClickListener(){ adapterView: AdapterView<*>, view1: View, i: Int, l: Long ->
             var i : Intent = Intent(this, ViewExercicesActivity::class.java)
             startActivity(i)
@@ -61,14 +39,13 @@ class MainExerciciActivity : ListActivity() {
     }
     fun añadir(s : String){
         musculos.add(s)
-        Log.d("LOLLLLLLLLLLLL", musculos[0])
         ListarMusclus()
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (data != null) {
             musculo = data.getStringExtra("musculo")
-            Log.d("AQQQQUUUUUUUUÏIIIIIIII", musculo)
         }
         añadir(musculo)
     }
