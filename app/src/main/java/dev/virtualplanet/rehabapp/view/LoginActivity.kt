ee35ca83daf17244faf32cadb2b63d1ca4913f2d
DarkDhz <blazegammer24@gmail.com>
2020-03-29 17:37:48 +0200

    A-S2-01-Login and register database implemented

--- app/src/main/java/dev/virtualplanet/rehabapp/view/LoginActivity.kt
+++ app/src/main/java/dev/virtualplanet/rehabapp/view/LoginActivity.kt
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import dev.virtualplanet.rehabapp.R
 import dev.virtualplanet.rehabapp.controller.Controller
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class LoginActivity : AppCompatActivity() {
 
     private val controller = Controller
+
+    private val data = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +31,45 @@ class LoginActivity : AppCompatActivity() {
         //BLOCKED FOR SECURITY
     }
 
-    fun goToMainActivity(view: View) {
-        val intent = Intent (this, MainActivity::class.java)
-        startActivity(intent)
+    fun login(view: View) {
+        val mail = this.username_login.text.toString()
+        val pass = this.password_login.text.toString()
+
+        val users = data.collection("USERS")
+
+        /*users.document("test").set(mapOf(
+            "name" to "Martin Garrix",
+            "mail" to "test",
+            "password" to "1234abcd",
+            "sex" to "man",
+            "age" to 23,
+            "weight" to 80,
+            "height" to 180,
+            "wheel" to false
+        ))*/
+
+        if (mail != "") {
+            data.collection("USERS").document(mail)
+                .get().addOnSuccessListener {
+                    val check_pass = it.get("password").toString()
+                    if (check_pass == pass) {
+                        val message = Toast.makeText(applicationContext, "Contraseña correcta", Toast.LENGTH_LONG)
+                        message.show()
+                        val intent = Intent (this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        val message = Toast.makeText(applicationContext, "Contraseña incorrecta", Toast.LENGTH_LONG)
+                        message.show()
+                    }
+                }.addOnFailureListener {
+                    val message = Toast.makeText(applicationContext, "Contraseña o usuario incorrecto", Toast.LENGTH_LONG)
+                    message.show()
+                }
+
+        } else {
+            val message = Toast.makeText(applicationContext, "Alguno de los campos esta vacio", Toast.LENGTH_LONG)
+            message.show()
+        }
     }
 
     fun goToRecoverPass(view: View) {
