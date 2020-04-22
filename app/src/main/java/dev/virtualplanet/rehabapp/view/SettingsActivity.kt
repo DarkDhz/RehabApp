1744b10a34fa19aa7a819bc8c6380ed3cce8cb1b
DarkDhz <blazegammer24@gmail.com>
2020-04-22 13:58:14 +0200

    A-S2-19-Requiered confirmation to logout

--- app/src/main/java/dev/virtualplanet/rehabapp/view/SettingsActivity.kt
+++ app/src/main/java/dev/virtualplanet/rehabapp/view/SettingsActivity.kt
@@ -3,8 +3,12 @@ package dev.virtualplanet.rehabapp.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 import android.widget.Switch
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import dev.virtualplanet.rehabapp.R
 import dev.virtualplanet.rehabapp.controller.Controller
 import kotlinx.android.synthetic.main.activity_settings.view.*
@@ -35,7 +39,23 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     fun logOut(view: View) {
-        Controller.removeUser(this)
+        var inflater : LayoutInflater = layoutInflater
+        val builder = AlertDialog.Builder(this, R.style.AlertDialog)
+        builder.setView(inflater.inflate(R.layout.delete_muscle_alert, null))
+        val dialog: AlertDialog = builder.create()
+        var alertView = inflater.inflate(R.layout.delete_muscle_alert, null)
+        alertView.findViewById<TextView>(R.id.delete_muscle_alert_header).text = "¿Estás seguro que quieres cerrar sesión?"
+        alertView.findViewById<Button>(R.id.delete_muscle_alert_cancel).setOnClickListener {
+            dialog.cancel()
+        }
+        alertView.findViewById<Button>(R.id.delete_muscle_alert_confirm).setOnClickListener {
+            dialog.cancel()
+            Controller.removeUser(this)
+        }
+
+        dialog.setView(alertView)
+        dialog.show()
+
 
     }
 
